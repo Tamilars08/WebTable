50a41b767ace2b0bf0c026224d026e6dde5c782a
Tamilars08 <tamilrajendiran1997@gmail.com>
2022-08-25 00:57:16 +0530

    addWebtable

---
+++ WebTable.java
@@ -0,0 +1,41 @@
+package com.example.webtable;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebTable{
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\OneDrive\\Documents\\workspace-spring-tool-suite-4-4.13.1.RELEASE\\AutomationWithSelenium\\Driver\\chromedriver.exe");
+	    WebDriver driver = new ChromeDriver();
+	    driver.manage().window().maximize();
+	    driver.navigate().to("https://www.w3schools.com/html/html_tables.asp");
+	    WebElement webTable = driver.findElement(By.id("customers"));
+	    List<WebElement> allData = webTable.findElements(By.tagName("tr"));
+	    
+	   for (int i=0; i<allData.size(); i++) {
+		   WebElement element = allData.get(i);
+		   String text = element.getText();
+		   System.out.println(text);
+//           String attribute = element.getAttribute("Value");	
+//           System.out.println(attribute);
+		   System.err.println("WebTable Edited");
+//           
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+}
